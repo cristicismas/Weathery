5836c7972c73ae87e8be040c34350cacc2ed535f
Cristi <cristicismas99@gmail.com>
2018-10-22 18:08:50 +0300

    Remove client build from .gitignore and track it

---
+++ client/build/precache-manifest.941181b89db5a4270fa046d253ee7da1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ffdc844fe37edf7dcb21",
+    "url": "/static/js/main.ffdc844f.chunk.js"
+  },
+  {
+    "revision": "0ce163a4060d0330cfc5",
+    "url": "/static/js/1.0ce163a4.chunk.js"
+  },
+  {
+    "revision": "ffdc844fe37edf7dcb21",
+    "url": "/static/css/main.18a00eac.chunk.css"
+  },
+  {
+    "revision": "9b7e3d66000d66c2a836988488ecaa7a",
+    "url": "/index.html"
+  }
+];
