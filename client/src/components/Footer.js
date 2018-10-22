bdfd1f193e6ff517a80e179cf37552fc82e82ad2
Cristi <cristicismas99@gmail.com>
2018-10-22 16:00:55 +0300

    Add noopener and noreferrer to footer links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,8 +5,8 @@ class Footer extends Component {
   render() {
     return (
       <section id='footer'>
-        <p>Weather data from <a href='https://www.apixu.com/' target='_blank'>APIXU</a></p>
-        <p>Made by <a href='https://cristicismas.github.io/' target='_blank'>Cristi Cismas</a></p>
+        <p>Weather data from <a href='https://www.apixu.com/' target='_blank' rel="noopener noreferrer">APIXU</a></p>
+        <p>Made by <a href='https://cristicismas.github.io/' target='_blank' rel="noopener noreferrer">Cristi Cismas</a></p>
       </section>
     );
   }
