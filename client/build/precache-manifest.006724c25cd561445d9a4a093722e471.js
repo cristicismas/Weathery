f5ba43adc075c457ddea038a84cc5b7e1ffaf21d
Cristi <cristicismas99@gmail.com>
2018-10-25 17:41:28 +0300

    Rebuild

--- client/build/precache-manifest.006724c25cd561445d9a4a093722e471.js
+++ client/build/precache-manifest.006724c25cd561445d9a4a093722e471.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/weathery/static/js/runtime~main.339e1877.js"
   },
   {
-    "revision": "a32ae516da887a49aef5",
-    "url": "/weathery/static/js/main.a32ae516.chunk.js"
+    "revision": "552c91b64a8881fb0f88",
+    "url": "/weathery/static/js/main.552c91b6.chunk.js"
   },
   {
     "revision": "b74d208e6fb47aacc3bb",
     "url": "/weathery/static/js/1.b74d208e.chunk.js"
   },
   {
-    "revision": "a32ae516da887a49aef5",
+    "revision": "552c91b64a8881fb0f88",
     "url": "/weathery/static/css/main.18a00eac.chunk.css"
   },
   {
-    "revision": "0730dae4978b192def6ab6798e81535f",
+    "revision": "164f7269524fa12641efa281eac42b32",
     "url": "/weathery/index.html"
   }
 ];
